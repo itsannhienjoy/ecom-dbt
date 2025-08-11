121c1aba89e2e482b9666a6507719fdb24010aa5
itsannhienjoy <dophuongannhien3105@gmail.com>
2025-08-12 00:13:51 +1000

    fix model pk

--- ecom_dbt/models/marts/fct/fct_orders.sql
+++ ecom_dbt/models/marts/fct/fct_orders.sql
@@ -29,24 +29,22 @@ src as (
   from {{ ref('stg_orders') }} o
 ),
 
-final as (
-  select
-    s.order_id,
-    c.customer_pk,
-    d.date_pk as order_date_pk,
-    os.order_status_pk,
-    coalesce(rev.total_revenue, 0) as total_revenue,
-    coalesce(pay.total_paid, 0) as total_paid,
-    coalesce(pay.payment_count, 0) as payment_count,
-    t.time_pk as order_time_pk
-  from src s
-  left join payments_rollup pay on s.order_id = pay.order_id
-  left join revenue_rollup rev on s.order_id = rev.order_id
-  left join {{ ref('dim_customer') }} c on s.customer_id = c.customer_id
-    and s.purchase_ts between c.valid_from and coalesce(c.valid_to, '2999-12-31')
-  left join {{ ref('dim_date') }} d on cast(s.purchase_ts as date) = d.date_key
-  left join {{ ref('dim_order_status') }} os on s.order_status = os.order_status
-  left join {{ ref('dim_time') }} t
-    on to_time(s.purchase_ts) = t.time_key
-)
-select * from final
+select
+  s.order_id as order_pk,
+  s.order_id,
+  c.customer_pk,
+  d.date_pk as order_date_pk,
+  os.order_status_pk,
+  coalesce(rev.total_revenue, 0) as total_revenue,
+  coalesce(pay.total_paid, 0) as total_paid,
+  coalesce(pay.payment_count, 0) as payment_count,
+  t.time_pk as order_time_pk
+from src s
+left join payments_rollup pay on s.order_id = pay.order_id
+left join revenue_rollup rev on s.order_id = rev.order_id
+left join {{ ref('dim_customer') }} c on s.customer_id = c.customer_id
+  and s.purchase_ts between c.valid_from and coalesce(c.valid_to, '2999-12-31')
+left join {{ ref('dim_date') }} d on cast(s.purchase_ts as date) = d.date_key
+left join {{ ref('dim_order_status') }} os on s.order_status = os.order_status
+left join {{ ref('dim_time') }} t
+  on to_time(s.purchase_ts) = t.time_key
